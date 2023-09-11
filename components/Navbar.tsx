6a26143aef95eeb71c88a4d9066efb7d45f8b127
Alejandro <alejandroaml0528@gmail.com>
2023-09-11 13:06:20 -0400

    navbar and sidebar with typescript

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,9 @@ import { BsHouseDoor, BsPerson, BsGear } from "react-icons/bs";
 import Image from "next/image";
 import logoImage from "../public/logo.png";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white p-4 shadow-md flex justify-between items-center">
+    <nav className="bg-white shadow-md flex justify-between items-center">
       {/* Izquierda (imagen del logo) */}
       <div className="flex items-center">
         <Image
@@ -18,13 +18,13 @@ const Navbar = () => {
 
       {/* Centro (texto "Administración / Inicio") */}
       <div className="text-center">
-        <p className="font-bold text-gray-700 text-base">
+        <p className="font-bold text-gray-700 text-lg">
           Administración / Inicio
         </p>
       </div>
 
       {/* Derecha (botones) */}
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-2 mr-2">
         <button className="text-gray-700 mx-1">
           <BsHouseDoor className="text-2xl" />
         </button>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,40 +1,48 @@
 "use client"
 import React, { useState } from "react";
 import {
-    BsBoxArrowLeft, // Nuevo icono para cerrar el sidebar
+    BsBoxArrowLeft,
     BsBoxArrowRight,
     BsFillHouseLockFill,
-    BsFillHousesFill
+    BsFillHousesFill,
 } from "react-icons/bs";
-import { FaWarehouse, FaHospital, FaTags, FaMapMarkedAlt, FaBriefcaseMedical, FaUsers, FaFileAlt } from "react-icons/fa";
-import { Josefin_Sans } from 'next/font/google'
+import {
+    FaWarehouse,
+    FaHospital,
+    FaTags,
+    FaMapMarkedAlt,
+    FaBriefcaseMedical,
+    FaUsers,
+    FaFileAlt,
+} from "react-icons/fa";
+import { Josefin_Sans } from "next/font/google";
 
-const josefin_sans = Josefin_Sans({
-    weight: ['400'],
+const josefin_sans = Josefin_Sans({  //Fuente de google fonts configurando
+    weight: ["400"],
     subsets: ["latin"],
-})
-const Sidebar = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [activeSection, setActiveSection] = useState("inicio"); // Estado para rastrear la sección activa
+});
 
-    const toggleSidebar = () => {
+const Sidebar: React.FC = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [activeSection, setActiveSection] = useState("inicio");
+
+    const toggleSidebar = () => {           //Funcion para abrir y cerrar el sidebar
         setIsSidebarOpen(!isSidebarOpen);
     };
 
-    // Función para manejar el clic en una sección
-    const handleSectionClick = (sectionName) => {
+    const handleSectionClick = (sectionName: string) => { //Funcion manejedaora para cambiar de seccion
         setActiveSection(sectionName);
     };
 
-    const activeColor = "text-color3 bg-slate-100 rounded-md"; // Define el color activo aquí
+    const activeColor = "text-color3 bg-slate-100 rounded-md";
     const hoverActiveColor = ""; // Color de hover para secciones activas
     const hoverInactiveColor = "hover:text-color5"; // Color de hover para secciones inactivas
-
 
     return (
         <aside
             className={`bg-color3 text-white ${isSidebarOpen ? "w-72" : "w-20"
-                } transition-all duration-300 ease-in-out`}>
+                } transition-all duration-300 ease-in-out`}
+        >
             {isSidebarOpen && (
                 <div className={`${josefin_sans.className} py-6 px-4 flex bg-blue-900`}>
                     <h2 className="text-center text-xl border-b border-white">ADMINISTRACION VENSALUD</h2>
@@ -58,7 +66,10 @@ const Sidebar = () => {
             <ul className="ml-2 my-4 space-y-4 flex flex-col justify-start">
                 <li>
                     <div
-                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "inicio" ? `${activeColor} ${hoverActiveColor}` : hoverInactiveColor}`}
+                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "inicio"
+                            ? `${activeColor} ${hoverActiveColor}`
+                            : hoverInactiveColor
+                            }`}
                         onClick={() => handleSectionClick("inicio")}
                     >
                         <BsFillHousesFill className="inline-block mr-3 h-6 w-6" />
@@ -69,7 +80,10 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <div
-                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "inventario" ? `${activeColor} ${hoverActiveColor}` : hoverInactiveColor}`}
+                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "inventario"
+                            ? `${activeColor} ${hoverActiveColor}`
+                            : hoverInactiveColor
+                            }`}
                         onClick={() => handleSectionClick("inventario")}
                     >
                         <FaWarehouse className="inline-block mr-3 h-6 w-6" />
@@ -80,7 +94,10 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <div
-                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "centros_de_salud" ? `${activeColor} ${hoverActiveColor}` : hoverInactiveColor}`}
+                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "centros_de_salud"
+                            ? `${activeColor} ${hoverActiveColor}`
+                            : hoverInactiveColor
+                            }`}
                         onClick={() => handleSectionClick("centros_de_salud")}
                     >
                         <FaHospital className="inline-block mr-3 h-6 w-6" />
@@ -91,7 +108,10 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <div
-                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "marcas" ? `${activeColor} ${hoverActiveColor}` : hoverInactiveColor}`}
+                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "marcas"
+                            ? `${activeColor} ${hoverActiveColor}`
+                            : hoverInactiveColor
+                            }`}
                         onClick={() => handleSectionClick("marcas")}
                     >
                         <FaTags className="inline-block mr-3 h-6 w-6" />
@@ -102,7 +122,10 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <div
-                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "municipios" ? `${activeColor} ${hoverActiveColor}` : hoverInactiveColor}`}
+                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "municipios"
+                            ? `${activeColor} ${hoverActiveColor}`
+                            : hoverInactiveColor
+                            }`}
                         onClick={() => handleSectionClick("municipios")}
                     >
                         <FaMapMarkedAlt className="inline-block mr-3 h-6 w-6" />
@@ -113,7 +136,10 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <div
-                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "bajas_tecnicas" ? `${activeColor} ${hoverActiveColor}` : hoverInactiveColor}`}
+                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "bajas_tecnicas"
+                            ? `${activeColor} ${hoverActiveColor}`
+                            : hoverInactiveColor
+                            }`}
                         onClick={() => handleSectionClick("bajas_tecnicas")}
                     >
                         <FaBriefcaseMedical className="inline-block mr-3 h-6 w-6" />
@@ -124,7 +150,10 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <div
-                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "usuarios" ? `${activeColor} ${hoverActiveColor}` : hoverInactiveColor}`}
+                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "usuarios"
+                            ? `${activeColor} ${hoverActiveColor}`
+                            : hoverInactiveColor
+                            }`}
                         onClick={() => handleSectionClick("usuarios")}
                     >
                         <FaUsers className="inline-block mr-3 h-6 w-6" />
@@ -135,7 +164,10 @@ const Sidebar = () => {
                 </li>
                 <li>
                     <div
-                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "reportes" ? `${activeColor} ${hoverActiveColor}` : hoverInactiveColor}`}
+                        className={`mx-2 p-2 text-lg font-semibold cursor-pointer flex ${activeSection === "reportes"
+                            ? `${activeColor} ${hoverActiveColor}`
+                            : hoverInactiveColor
+                            }`}
                         onClick={() => handleSectionClick("reportes")}
                     >
                         <FaFileAlt className="inline-block mr-3 h-6 w-6" />
