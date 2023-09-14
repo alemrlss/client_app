3dac9c6e3e5f8f271a9fb867e66a89c5978a698d
Alejandro <alejandroaml0528@gmail.com>
2023-09-14 19:22:06 -0400

    test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import { BsHouseDoor, BsPerson, BsGear } from "react-icons/bs";
 import Image from "next/image";
-import logoImage from "../public/logo.png";
+import logoImage from "@/public/images/navbar/logo2.png";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white shadow-md flex justify-between items-center">
-      {/* Izquierda (imagen del logo) */}
-      <div className="flex items-center">
-        <Image
-          src={logoImage}
-          alt="Logo de la empresa"
-          width={80}
-          height={40}
-        />
-      </div>
-
-      {/* Centro (texto "Administración / Inicio") */}
-      <div className="text-center">
+    <nav className="bg-white shadow-md flex justify-between items-center p-2">
+      <div className="flex items-center pl-2">
         <p className="font-bold text-gray-700 text-lg">
           Administración / Inicio
         </p>
+      </div>
+
+
+      <div className="text-center">
+
+        <Image
+          className="rounded shadow-sm shadow-black"
+          src={logoImage}
+          alt="Logo de la empresa"
+          width={200}
+          height={100}
+        />
       </div>
 
       {/* Derecha (botones) */}
